f4ca3fb169c86160c0dce5b15243c66d92438643
RichoDemus <richodemus@gmail.com>
2019-08-15 14:51:20 +0200

    Arc and mutex

--- README.md
+++ README.md
@@ -11,6 +11,6 @@ cargo clippy
 ```
 
 
-[Where I'm currently](file:///Users/rtjerngren/.rustup/toolchains/stable-x86_64-apple-darwin/share/doc/rust/html/book/ch16-03-shared-state.html)
+[Where I'm currently](file:///Users/rtjerngren/.rustup/toolchains/stable-x86_64-apple-darwin/share/doc/rust/html/book/ch16-04-extensible-concurrency-sync-and-send.html)
 
 [Future read: The Rustonomicon](https://doc.rust-lang.org/stable/nomicon/)
--- lib/src/concurrency/threads.rs
+++ lib/src/concurrency/threads.rs
@@ -1,6 +1,6 @@
 use std::thread;
 use std::time::Duration;
-use std::sync::mpsc;
+use std::sync::{mpsc, Mutex, Arc};
 
 pub(crate) fn threads() {
     let handle = thread::spawn(|| {
@@ -36,4 +36,36 @@ pub(crate) fn threads() {
 
     let received = rx.recv().unwrap();
     println!("Got: {}", received);
+    mutex()
+}
+
+fn mutex(){
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+
+    // accessing data across multiple threads with Arc
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
